ca21e479629875a5868ea5b3d240de0cfe0d4231
李永磊 <liylo@chanjet.com>
2017-11-29 11:20:46 +0800

    增加了对小圆点显示计数的支持

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,28 +1,47 @@
 /**
  * Created by Liyongleihf2006
  *一个极其简单的轮播图,只支持mouseenter和mouseleave触发切换,要改成其他切换其实很简单,改代码吧哈
- *params:
- *       container 轮播的容器
- *       snaps 快照的数组,具体看demo就好了
- *       interval 轮播的切换速度,单位是毫秒,默认是1000
+ *params:{
+ *         container:element 轮播的容器
+ *         snaps:array 快照的数组,具体看demo就好了
+ *         interval:number 轮播的切换速度,单位是毫秒,默认是1000
+ *         space:number default:24 小圆点偏移的像素大小
+ *         circleSize default:12 小圆点的大小,默认是12px,是在css中设置的,这里设置了可以覆盖css中的设置
+ *         showNo:boolean default:true 是否显示小圆点上面的计数
+ *       }
  */
-function carousel(container,snaps,interval){
+function carousel(params){
+    /* 轮播的容器 */
+    var container  = params.container;
+    /* 快照的数组 */
+    var snaps = params.snaps;
+    /* 多久自动变换一次,默认是1秒 */
+    var interval = params.interval||1000;
+    /* 小圆点偏移的单位宽度,以像素为单位,这里是24px */
+    var space = params.space||24;
+    /* 小圆点的大小,默认是12px,是在css中设置的,这里设置了可以覆盖css中的设置 */
+    var circleSize = params.circleSize;
+    /* 是否显示小圆点上面的计数 */
+    var showNo = params.showNo===undefined?true:params.showNo;
     /* 暂停的标志,当鼠标悬浮该组件的时候就会暂停 */
     var suspend = false;
-    /* 多久自动变换一次,默认是1秒 */
-    var interval = interval||1000;
     /* 当前显示的快照在数组中的位置 */
     var currentIdx = 0;
     /* 生成轮播组件的元素原来的class */
     var originContainerCls = container.getAttribute("class")||"";
     /* 快照数组的长度,用来计算下方每个小原点的位置的 */
     var snapsCount = snaps.length;
-    /* 小圆点偏移的单位宽度,以像素为单位,这里是24px */
-    var space = 24;
+    
     /* 所有小圆点的中心,用来计算下方每个小原点的位置的 */
     var middlePositionOrder = (snapsCount-1)/2;
     container.innerHTML="";
-    container.setAttribute("class",originContainerCls+" carousel-container");
+    container.setAttribute("class",function(){
+        var containerCls = originContainerCls+" carousel-container";
+        if(showNo){
+            containerCls+=" carousel-show-no";
+        }
+        return containerCls;
+    }());
     container.addEventListener("mouseenter",function(){
         suspend=true;
     });
@@ -46,7 +65,13 @@ function carousel(container,snaps,interval){
         var carouselHandle=document.createElement("div");
         carouselItem.appendChild(carouselHandle);
         carouselHandle.setAttribute("class","carousel-handle");
-        carouselHandle.setAttribute("style","left:calc(50% + "+(idx-middlePositionOrder)*space+"px)");
+        carouselHandle.setAttribute("style",function(){
+            var carouselHandleStyle = "left:calc(50% + "+(idx-middlePositionOrder)*space+"px);";
+            if(circleSize){
+                carouselHandleStyle+="height:"+circleSize+"px;width:"+circleSize+"px";
+            }
+            return carouselHandleStyle;
+        }());
         carouselHandle.addEventListener("mouseover",function(){
             currentIdx=idx;
             doCheck();
